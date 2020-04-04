28188b9836edf948609edce97700264eba1ce05a
manicalo <lorenzomanica@gmail.com>
2020-04-04 19:33:42 -0300

    Add navigation component

--- app/src/main/java/br/pro/lmit/androidjavaevents/EventActivity.java
+++ app/src/main/java/br/pro/lmit/androidjavaevents/EventActivity.java
@@ -3,12 +3,15 @@ package br.pro.lmit.androidjavaevents;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
 
-public class MainActivity extends AppCompatActivity {
+public class EventActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Navigation.findNavController(this, R.id.nav_host);
     }
 }
